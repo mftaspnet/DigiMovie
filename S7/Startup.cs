6ec97d7e9fb9c5ab514388d0c4fefd367faa6d55
MFTASPNET <MFTASPNET@GMAIL.COM>
2017-08-06 20:25:33 +0430

    Primary

---
+++ S7/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(S7.Startup))]
+namespace S7
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
